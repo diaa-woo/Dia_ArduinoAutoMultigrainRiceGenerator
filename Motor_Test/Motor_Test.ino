3f6ad4d6960ed8db433a497738f37c17248a0dc8
diadntjr <dntjr0501@gmail.com>
2022-02-28 11:30:59 +0900

    Complete control motor dirction

--- Motor_Test/Motor_Test.ino
+++ Motor_Test/Motor_Test.ino
@@ -1,18 +1,38 @@
+extern volatile unsigned long timer0_millis;
+
 #define CW 3
 #define CLK 2
+#define button 4
+
+int cw = 1;
+int clk = 0;
+char input = 0;
+
+unsigned long past = 0;
+int flag = 0;
+unsigned long time = millis();
 
 void setup() {
+  Serial.begin(9600);
   pinMode(CW, OUTPUT);
   pinMode(CLK, OUTPUT);
-  digitalWrite(CW, 0);
-  digitalWrite(CLK, LO);
+  pinMode(button, INPUT);
+  digitalWrite(CW, cw);
+  digitalWrite(CLK, 1);
 }
 
 void loop() {
-  
-  digitalWrite(CLK, HIGH);
+  input = Serial.read();
+  if(input=='1') {
+    Serial.print("on");
+    digitalWrite(CLK,0);
+    delay(500);
+    cw = !cw;
+    digitalWrite(CLK, 1);
+    digitalWrite(CW, cw);
+  }
+  digitalWrite(CLK, 1);
   delayMicroseconds(1000);
-  digitalWrite(CLK, LOW);
+  digitalWrite(CLK, 0);
   delayMicroseconds(1000);
-
 }
